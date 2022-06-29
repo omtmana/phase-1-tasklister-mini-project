2702a1baebdfc5b4e98a427c653057ae6a91e683
Olivia <oliviatmanalastas@gmail.com>
2022-06-29 13:55:08 -0400

    Added Event Listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+// create an array for the 
+// const taskList = [];
+// const newBtn = document.querySelector('submitBtn')
+
+const taskList = [] //this is where the values of the list goes
+const taskDiv = document.querySelector('#tasks');
+const newBtn = document.querySelector('#newSubmitBtn');
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+newBtn.addEventListener('click', async (SubmitEvent) => {
+  SubmitEvent.preventDefault()
+
+  let string = document.getElementById('#new-task-description').value
+  console.log('Task entered', string)
+  taskList.push(string)
+})
+
